ab4ff0c691f859fcded1aed2fd02816c1d9e2b07
M Nguyen <38267385+minhnnguyen29@users.noreply.github.com>
2023-03-21 15:05:37 +1100

    App now can resolve templates by removing line in app.properities

--- src/main/java/com/example/project01/ApplicationController.java
+++ src/main/java/com/example/project01/ApplicationController.java
@@ -1,20 +1,20 @@
 package com.example.project01;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
+@Controller
 public class ApplicationController {
     @Value("${spring.application.name}")
     String appName; 
 
-    @GetMapping(path="/")
+    @GetMapping(value="/")
     public String homePage(Model model){
         model.addAttribute("appName", appName); 
-        return "home";
+        return "index";
     }
     
 }
