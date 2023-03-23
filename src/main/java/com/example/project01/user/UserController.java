091a07ecd4cb9eade37acc52f1aac3aaf3aceed9
M Nguyen <38267385+minhnnguyen29@users.noreply.github.com>
2023-03-23 16:24:48 +1100

    Add signupForm + register templates. Started working on functions in Controller

--- src/main/java/com/example/project01/user/UserController.java
+++ src/main/java/com/example/project01/user/UserController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,25 +23,31 @@ public class UserController {
     @Autowired 
     private UserService userService; //create a reference to service layer 
 
-    //============================================END POINTS===================================================
-    //POST
+    // ========================== CREATE ============================
+    
+    @GetMapping(path = "/signupForm")
+    public String showSignupForm(User newUser){
+        return "signupForm"; 
+    }
+
     @PostMapping(path = "/register") //MAP a POST request to add a new user 
-    public void register(@RequestBody User newUser)
+    public String register(@Valid User newUser, BindingResult result, Model model)
     {
-        if (newUser != null) 
-        {
-            userService.addNewUser(newUser);
+        if(result.hasErrors()) { //if newUser is not valid (BindingResult unsuccessful)
+            return "signupForm"; //show signupForm again 
         }
+        return "redirect:/home";//returns template 'register' that allows user to sign up 
     }
 
 
-    //GET 
+
+    // ========================== READ ============================== 
     @GetMapping(value = "/all")
     public String showUsers(Model model)
     {   
         //bind attr 'allUsers' with value of all users
         model.addAttribute("allUsers", userService.findAllUsers());
-        return "all"; 
+        return "all";//returns template 'all' that display the table listing all users 
     }
 
     @GetMapping(path = "/{id}")
@@ -47,7 +56,7 @@ public class UserController {
         return userService.findThisUser(id); 
     }
 
-    //PUT 
+    // ========================= UPDATE ===================================     
     @PutMapping(path = "/update/{id}")
     public void updateUser(@PathVariable("id") Long id, 
                             @RequestParam(required = false) String password) 
@@ -55,7 +64,7 @@ public class UserController {
         userService.updateUserDetails(id, password);    
     }
 
-    //DELETE 
+    // ======================== DELETE ==================================== 
     @DeleteMapping(path = "/delete/{id}")
     public void deleteUser(@PathVariable("id") Long id)
     {
